e0533888a5a0dd2be7a1f9262ce8ade59063065f
shekhar <shekhar@test.com>
2023-10-21 12:43:03 +0530

    Adding repo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Card from './Card'
+import {MyHeader} from './Header'
+
+function App() {
+
+  const cardInfo = {
+    title: "GREEN CARD",
+    description :"this is demo test that we are showing this is demo test that we are showing"
+  }
+
+  const linkText = {
+    home :"Homeee",
+    price : "Pricinggg",
+    features: "Featuresss"
+  }
+
+  return (
+    <div>
+      <MyHeader {...linkText}/>
+      <Card cardInfo={cardInfo} btnText="Click Me" />
+      {/* <h1>{cardInfo.title}</h1>
+      <b>{cardInfo.description}</b> */}
+    </div>
+  );
+}
+
+export default App;
