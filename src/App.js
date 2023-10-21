d4a7e4e494ede949fe479b58a8bf6500bffdf574
shekhar <shekhar@test.com>
2023-10-21 12:48:35 +0530

    clean code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import './App.css';
-import Card from './Card'
-import {MyHeader} from './Header'
 
 function App() {
 
-  const cardInfo = {
-    title: "RED CARD",
-    description :"this is demo test that we are showing this is demo test that we are showing"
-  }
-
-  const linkText = {
-    home :"Homeee",
-    price : "Pricinggg",
-    features: "Featuresss"
-  }
-
   return (
     <div>
-      <MyHeader {...linkText}/>
-      <Card cardInfo={cardInfo} btnText="Click Me" />
-      {/* <h1>{cardInfo.title}</h1>
-      <b>{cardInfo.description}</b> */}
+     Hello
     </div>
   );
 }
