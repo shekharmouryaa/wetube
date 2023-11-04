0fe02664087ef9f87305bb4d7853b38713841e03
shekhar <shekhar@test.com>
2023-11-04 11:51:05 +0530

    Managing State

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Card = (props) => {
+    const {title, content, setCount, count} = props
+    return (
+        <div className='m-4'>
+            <div className="card" style={{width: "300px"}} >
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{content}</p>
+                    <p>
+                    <strong>{count}</strong>
+                    </p>
+                    <button className="btn btn-primary" 
+                    onClick={()=> setCount(count + 1)}>Increase</button>
+                    <button className="btn btn-success mx-2" 
+                    onClick={()=> setCount(count - 1)}>Decrease</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,32 +1,53 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { Card } from './Card'
 
 export const Users = () => {
-    const [username, setUsername] = useState("Shekhar")
-    const [count, setCount] = useState(1)
 
-    useEffect(
-        () => {
-            setCount(10)
-            setUsername("Shashank")
+    const username = "Shekhar";  // Immutable state
+    const [count , setCount] = useState(0) // Mutable state - Hook useState
 
-            return () => {
-                setUsername("Shekhar")
-            }
+    // const [name , setName] = useState("shekhar") // Mutable state - Hook useState
+    // const [age , setAge] = useState(20) // Mutable state - Hook useState
+    // const [city , setCity] = useState("Indore") // Mutable state - Hook useState
 
-        }, [])
+    const [info , setInfo] = useState({
+        name : "Shekhar",
+        age :  20,
+        city: "Indore"
+    })
 
 
-    const changeName = () => {
-        setUsername("Himanshu")
-    }
 
+
+    console.log("reder");
     return (
-        <>
-            <div>User Name : {username}</div>
-            <div>Count : {count}</div>
-            <button className='btn btn-outline-dark' onClick={changeName} >Change Name</button>
-            <button className='btn btn-outline-success'
-                onClick={() => setCount(count + 1)} >Change Count</button>
-        </>
+        <div className='m-3'>
+           <h2>Hello World !</h2>
+           <Card title={username} content="this is demo text"
+            count={count} setCount={setCount}/>
+            {/* <h3> User Name :{name}</h3>
+            <h3> User Age :{age}</h3> 
+            <h3> User city :{city}</h3> */}
+
+            <h6> User Name :{info.name}</h6>
+            <h6> User Age :{info.age}</h6>
+            <h6> User City :{info.city}</h6>
+
+            {/* <button className='btn btn-info' 
+            onClick={()=>setName("Shashank")} >Change Name</button>
+             <button className='btn-outline-dark btn mx-2' 
+            onClick={()=>setAge(22)} >Change Age</button>  
+            <button className='btn-outline-dark btn mx-2' 
+            onClick={()=>setCity("Pune")} >Change City</button> */}
+
+             <button className='btn btn-outline-info my-2' 
+            onClick={()=>setInfo({...info, name : "Shashank"})} >Change Name</button>
+
+             <button className='btn-outline-dark btn mx-2' 
+            onClick={()=>setInfo({...info, age: 24})} >Change Age</button>
+
+            <button className='btn-outline-primary btn mx-2' 
+            onClick={()=>setInfo({...info, city: "Pune"})} >Change City</button>
+        </div>
     )
 }
